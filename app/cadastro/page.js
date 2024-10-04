bf9a31296396559cbad4d84cdb15a0e24e0612c6
carlos alberto hardman junior <carloshardmanjr@hotmail.com>
2024-10-04 04:24:08 -0300

    configuração e funcionamento da api

--- app/cadastro/page.js
+++ app/cadastro/page.js
@@ -1,20 +1,21 @@
-"use client"
+// app/cadastro/page.js
+"use client";
 
 import Form from "@/component/form";
 import { useCadastro } from "@/hooks/useCadastro";
 
-
 export default function Cadastro() {
     const { email, setEmail, password, setPassword, erro, handleSubmit } = useCadastro();
+
     return (
         <div>
             <h2>Cadastro</h2>
+            {erro && <p style={{ color: 'red' }}>{erro.message || String(erro)}</p>}
             <Form
                 email={email}
                 setEmail={setEmail}
                 password={password}
                 setPassword={setPassword}
-                erro={erro}
                 handleSubmit={handleSubmit}
             />
         </div>
--- service/cadastroService.js
+++ service/cadastroService.js
@@ -1,6 +1,6 @@
 export async function cadastrarUsuario(dadosDoUsuario){
     //crio uma variavel que recebe os dados da api
-    const response = await fetch("https://27bc352d-8ef8-4b52-976f-c6126bf6661d-00-28ordud37745w.riker.replit.dev/register",{
+    const response = await fetch("http://localhost:3000/api/cadastro",{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(dadosDoUsuario)
